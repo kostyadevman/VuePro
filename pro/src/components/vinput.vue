2cf473b38e301e6cf4993a64eb025825cf4fabf2
jora <jora@email.ru>
2019-09-14 19:37:11 +0300

    Initial commit

---
+++ pro/src/components/vinput.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <input v-model="someVar">
+    <h1>{{ someVar }}</h1>
+  </div>
+</template>
+
+<script>
+import getRandomString from './getRandomStr.js'
+
+export default {
+  name: 'vinput',
+  data () {
+    return {
+      someVar: 'initial value'
+    }
+  },
+
+  methods: {
+    changeSomeVar () {
+      let self = this
+      setInterval(function () {
+        self.someVar = getRandomString()
+        console.log(self.someVar)
+      }, 10000)
+    }
+  },
+
+  mounted () {
+    this.changeSomeVar()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ pro/src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import vinput from '../components/vinput'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'vinput',
+      component: vinput
+    }
+  ]
+})
